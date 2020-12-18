ceccf54be1b182243592e01e0e752c0e148e3caf
LiamBartolini <liambartolini@gmail.com>
2020-12-18 14:31:39 +0100

    refactoring

--- Bartolini.Liam.4H.OrdinamentoIndicizzato/Program.cs
+++ Bartolini.Liam.4H.OrdinamentoIndicizzato/Program.cs
@@ -1,39 +1,11 @@
 ﻿using System;
-using System.Data;
 using System.IO;
-using System.Linq;
-using System.Text;
+using Bartolini.Liam._4H.OrdinamentoIndicizzato.Models;
 
 namespace Bartolini.Liam._4H.OrdinamentoIndicizzato
 {
-    class ConsoleDataFormatter
-    {
-        const int TableWidth = 80;
-        public static void PrintSeperatorLine()
-        {
-            Console.WriteLine(new string('-', TableWidth));
-        }
-
-        static public void PrintRows(params string[] columns)
-        {
-            int columnWidth = (TableWidth - columns.Length) / columns.Length;
-            const string seed = "|";
-
-            string row = columns.Aggregate("|", (separator, columnsText) => separator + GetCenterAllignedText(columnsText, columnWidth) + seed);
-            Console.WriteLine(row);
-        }
-
-        static string GetCenterAllignedText(string columnText, int columnWidth)
-        {
-            columnText = columnText.Length > columnWidth ? columnText.Substring(0, columnWidth - 3) + "..." : columnText;
-
-            return string.IsNullOrEmpty(columnText) ? new string(' ', columnWidth) : columnText.PadRight(columnWidth - ((columnWidth - columnText.Length) / 2)).PadLeft(columnWidth);
-        }
-    }
-
     class Program
     {
-        
         static void Main(string[] args)
         {
             Console.WriteLine("Bartolini Liam Ordinamento indicizzato 18/12/2020\n");
